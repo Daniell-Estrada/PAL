f49ede4e547d2866a60dedc05e61698b916c1544
daniell <daniell@kali.kali>
2025-05-22 01:04:49 -0500

    [feat: error] correcion en EnrollmentRepository

--- src/main/java/com/example/pal/repository/EnrollmentRepository.java
+++ src/main/java/com/example/pal/repository/EnrollmentRepository.java
@@ -1,9 +1,10 @@
 package com.example.pal.repository;
 
+import com.example.pal.model.Course;
 import com.example.pal.model.Enrollment;
 import com.example.pal.model.User;
-
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,10 @@ import org.springframework.stereotype.Repository;
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
   List<Enrollment> findByCourseId(Long courseId);
 
+  Optional<Enrollment> findByStudentAndCourse(User student, Course course);
+
   boolean existsByStudentIdAndCourseId(Long studentId, Long courseId);
 
   List<Enrollment> findByStudentId(Long studentId);
 }
+
